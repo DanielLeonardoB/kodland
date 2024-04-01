6a4dff192276f98e1d0fcb51d43399a96f470e09
DanielLeonardoB <165502191+DanielLeonardoB@users.noreply.github.com>
2024-03-31 23:30:21 -0500

    Update traductor.py

--- traductor.py
+++ traductor.py
@@ -15,15 +15,26 @@ while respuesta == "S":
           print("Cómo deberíamos traducir " + eng_words[number])
           if input().capitalize() == sp_words[number]:
               print("¡¡¡Genial!!!")
-              score += 1
+              score += 1 
+              print("*"*10)
+              print(f"Puntuacion = {score}")
+              print(f"Ronda = {i+1}")
+              print("*"*10)
           else:
               print("No, no del todo... La palabra correcta es - " + sp_words[number])
-              score += 1
+              print("*"*10)
+              print(f"Puntuacion = {score}")
+              print(f"Ronda = {i+1}")
+              print("*"*10)
   else:
       word = input("Escribe una palabra en español: ").capitalize()
       translate = input("Escriba la traducción de esta palabra: ").capitalize()
       if len(word) > 0 and len(translate) > 0:
           sp_words.append(word)
+          eng_words.append(translate)
+          print("¡La palabra se ha añadido correctamente!")
+  respuesta = str(input("Quiere seguir jugando S/N ")).upper()
+
           eng_words.append(translate)
           print("¡La palabra se ha añadido correctamente!")
   respuesta = str(input("Quiere seguir jugando S/N ")).upper()
