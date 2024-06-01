655c673c5b5c3d4b74e2ad7249d7cc86c968e3f0
DanielLeonardoB <165502191+DanielLeonardoB@users.noreply.github.com>
2024-06-01 10:36:19 -0500

    Add files via upload
    pagina con links a sub paginas

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
-palabras = {"LOL": "una respuesta a algo gracioso", "CRINGE" :"algo raro o embarazoso",
-"ROFL": "una respuesta a una broma", "SHEESH" : "ligera desaprobación",
-"CREEPY": "aterrador, siniestro", "AGGRO": "ponerse agresivo/enojado"}
-print("Lista de palabras ")
-for i in palabras:
-    print(i)
-incognita = str(input("Ingrese una palabra que desea conocer su significado").upper())
-if incognita in palabras:
-   print("El significado de", incognita, "es:", palabras[incognita])
-else:
-    print("La palabra debe estar dentro de la lista")
-    
+from flask import Flask
+import random
+
+app = Flask(__name__)
+
+lista = [
+    "La mayoría de las personas que sufren adicción tecnológica experimentan un fuerte estrés cuando se encuentran fuera del área de cobertura de la red o no pueden utilizar sus dispositivos.",
+    "Según un estudio realizado en 2018, más del 50% de las personas de entre 18 y 34 años se consideran dependientes de sus smartphones."
+]
+
+@app.route("/")
+def inicio():
+    return '<a href="/dato">¡Ver un hecho al azar!</a><br><a href="/moneda">¡Lanzar una moneda!</a>'
+
+@app.route("/dato")
+def dato():
+    fact = random.choice(lista)
+    return f"<p>{fact}</p><br><a href='/'>Volver a inicio</a>"
+
+@app.route("/moneda")
+def moneda():
+    result = random.choice(["Cara", "Cruz"])
+    return f"<p>Resultado del lanzamiento: {result}</p><br><a href='/'>Volver a inicio</a>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
